5496cf1224cdafe2f418d010e72a17e572e2981e
Minh Quang <minhquangdn27@gmail.com>
2024-10-26 12:19:04 +0700

    test1

--- test2/test2/obj/Debug/net8.0/test2.AssemblyInfo.cs
+++ test2/test2/obj/Debug/net8.0/test2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211661137533941a23a8ac222d7420ae5b9ff3ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5b06f93d8e7b4902c407cc6885183d1c493a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("test2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
